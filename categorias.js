39400e74ec8b8ce64ffec79b5dd00c592efa9074
arielgilla <arielhgilla@gmail.com>
2026-09-16 09:49:00 -0300

    Create categorias.js

---
+++ categorias.js
@@ -0,0 +1,65 @@
+// categorias.js
+const categoriasJerarquicas = {
+  "Componentes de PC": {
+    "componentes-de-pc/motherboards/intel": "Motherboards Intel",
+    "componentes-de-pc/motherboards/amd": "Motherboards AMD",
+    "componentes-de-pc/microprocesadores/intel": "Microprocesadores Intel",
+    "componentes-de-pc/microprocesadores/amd": "Microprocesadores AMD",
+    "componentes-de-pc/memorias-ram/desktop": "Memorias RAM Desktop",
+    "componentes-de-pc/memorias-ram/notebook": "Memorias RAM Notebook",
+    "componentes-de-pc/placas-de-video": "Placas de Video",
+    "componentes-de-pc/placas-de-sonido": "Placas de Sonido",
+    "componentes-de-pc/placas-de-red": "Placas de Red",
+    "componentes-de-pc/discos-solidos-ssd": "Discos SSD",
+    "componentes-de-pc/discos-duros-mecanicos": "Discos Mecánicos",
+    "componentes-de-pc/fuentes": "Fuentes",
+    "componentes-de-pc/gabinetes": "Gabinetes",
+    "componentes-de-pc/refrigeracion": "Refrigeración",
+    "componentes-de-pc/pastas-termicas": "Pastas Térmicas",
+    "componentes-de-pc/combos-de-actualizacion": "Combos de Actualización"
+  },
+  "Periféricos": {
+    "perifericos/teclados": "Teclados",
+    "perifericos/mousepads": "Mousepads",
+    "perifericos/auriculares/gamer": "Auriculares Gamer",
+    "perifericos/auriculares/oficina-y-urbano": "Auriculares Oficina/Urbano",
+    "perifericos/microfonos": "Micrófonos",
+    "perifericos/webcams": "Webcams",
+    "perifericos/joysticks-volantes-y-simuladores": "Joysticks y Volantes",
+    "perifericos/audio/portatiles": "Audio Portátil",
+    "perifericos/placas-de-sonido": "Placas de Sonido"
+  },
+  "Monitores": {
+    "monitores/19-a-28-pulgadas": "Monitores 19-28\"",
+    "monitores/27-a-32-pulgadas": "Monitores 27-32\"",
+    "monitores/33-pulgadas-y-superior": "Monitores 33\"+",
+    "monitores/proyectores-y-pantallas": "Proyectores y Pantallas"
+  },
+  "Almacenamiento Portátil": {
+    "almacenamiento-portatil/pendrives": "Pendrives",
+    "almacenamiento-portatil/microsd": "MicroSD",
+    "almacenamiento-portatil/discos-externos": "Discos Externos"
+  },
+  "Impresión y Scanners": {
+    "impresion/impresoras-laser": "Impresoras Láser",
+    "impresion/inyeccion-de-tinta": "Impresoras Inkjet",
+    "impresion/termicas": "Impresoras Térmicas",
+    "impresion/impresion-3d": "Impresoras 3D",
+    "impresion/consumibles": "Consumibles"
+  },
+  "Redes y Conectividad": {
+    "redes/routers": "Routers",
+    "redes/switchs": "Switchs",
+    "redes/access-point": "Access Point",
+    "redes/modem-router": "Modem Router"
+  },
+  "Otros": {
+    "sillas-gamers": "Sillas Gamers",
+    "software": "Software",
+    "televisores": "Televisores",
+    "tablets": "Tablets",
+    "smartwatch": "Smartwatch",
+    "camaras-ip": "Cámaras IP",
+    "accesorios": "Accesorios"
+  }
+};
